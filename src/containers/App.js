d6403f838902f3560916d7994bc3f65611df56b9
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-08-25 16:38:25 -0400

    added new character button & some character info

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,27 +5,46 @@ class App extends Component {
     super(props);
       this.state = {
         name: '',
-        species: ''
+        gender: '',
+        homeworld: ''
       }
+      this.handleClick = this.handleClick.bind(this)
     }
 
     componentDidMount() {
-      let randomId = Math.floor(Math.random() * 88) + 1
+      let randomId = Math.floor(Math.random() * 87) + 1
       fetch(`https://swapi.co/api/people/${randomId}`)
       .then(response => response.json())
       .then(body => {
         this.setState({
           name: body.name,
-          species: body.species
+          gender: body.gender,
+          homeworld: body.homeworld
+        })
+      })
+    }
+
+    handleClick(event) {
+      let randomId = Math.floor(Math.random() * 87) + 1
+      fetch(`https://swapi.co/api/people/${randomId}`)
+      .then(response => response.json())
+      .then(body => {
+        this.setState({
+          name: body.name,
+          gender: body.gender,
+          homeworld: body.homeworld
         })
       })
     }
 
   render() {
     let name;
-    let species;
-    if (this.state.name !== undefined) {
+    let gender;
+    let homeworld;
+    if (this.state.name !== undefined && this.state.gender !== undefined && this.state.homeworld !== undefined) {
       name = this.state.name
+      gender = this.state.gender
+      homeworld = this.state.homeworld
     } else {
       name = 'Click the button to get a random character!'
     }
@@ -34,7 +53,8 @@ class App extends Component {
         <div className="small-12 large-6 small-centered columns text-center main">
           <h1>Star Wars Characters!</h1>
           <h3>{name}</h3>
-          <h5>{species}</h5>
+          <h3>{gender}</h3>
+          <h3>{homeworld}</h3>
           <button className="button" onClick={this.handleClick}>Check out a new character!</button>
         </div>
       </div>
